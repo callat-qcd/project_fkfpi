a0d2ea8bd1cbb52916c98921a32827fc2b46fca9
Jason Chang <pyo0220@gmail.com>
2018-05-01 04:18:05 +0900

    full data

--- fkfpi_lib.py
+++ fkfpi_lib.py
@@ -7,37 +7,48 @@ import matplotlib.pyplot as plt
 import chipt_awl as xpt
 
 ens_long = {
+    'a15m400' :'l1648f211b580m0217m065m838',
+    'a15m350' :'l1648f211b580m0166m065m838',
     'a15m310' :'l1648f211b580m013m065m838',
     'a15m220' :'l2448f211b580m0064m0640m828',
     'a15m130' :'l3248f211b580m00235m0647m831',
+    'a12m400' :'l2464f211b600m0170m0509m635',
+    'a12m350' :'l2464f211b600m0130m0509m635',
     'a12m310' :'l2464f211b600m0102m0509m635',
     'a12m220' :'l3264f211b600m00507m0507m628',
     'a12m220S':'l2464f211b600m00507m0507m628',
     'a12m220L':'l4064f211b600m00507m0507m628',
     'a12m130' :'l4864f211b600m00184m0507m628',
+    'a09m400' :'l3264f211b630m0124m037m440',
+    'a09m350' :'l3264f211b630m00945m037m440',
     'a09m310' :'l3296f211b630m0074m037m440',
     'a09m220' :'l4896f211b630m00363m0363m430',
 }
 r_a = {
+    'a15m400' :gv.gvar(2.059,0.023),
+    'a15m350' :gv.gvar(2.059,0.023),
     'a15m310' :gv.gvar(2.059,0.023),
     'a15m220' :gv.gvar(2.073,0.013),
     'a15m130' :gv.gvar(2.089,0.008),
+    'a12m400' :gv.gvar(2.575,0.017),
+    'a12m350' :gv.gvar(2.575,0.017),
     'a12m310' :gv.gvar(2.575,0.017),
     'a12m220S':gv.gvar(2.585,0.019),
     'a12m220' :gv.gvar(2.626,0.013),
     'a12m220L':gv.gvar(2.614,0.009),
     'a12m130' :gv.gvar(2.608,0.008),
+    'a09m400' :gv.gvar(3.499,0.024),
+    'a09m350' :gv.gvar(3.499,0.024),
     'a09m310' :gv.gvar(3.499,0.024),
     'a09m220' :gv.gvar(3.566,0.014)
     }
-L_ens = {'a15m310':16,'a15m220':24,'a15m130':32,
-    'a12m310':24,'a12m220':32,'a12m220S':24,'a12m220L':40,'a12m130':48,
-    'a09m310':32,'a09m220':48,}
+L_ens = {'a15m400':16,'a15m350':16,'a15m310':16,'a15m220':24,'a15m130':32,
+    'a12m400':24,'a12m350':24,'a12m310':24,'a12m220':32,'a12m220S':24,'a12m220L':40,'a12m130':48,
+    'a09m400':32,'a09m350':32,'a09m310':32,'a09m220':48,}
 
 def decay_constant(s,mval,gvdata):
     Fpi = gvdata['z0p_pion']*2*(mval['mq1']+gvdata['mresl'])/gvdata['e0_pion']**(3./2.)
     Fka = gvdata['z0p_kaon']*(mval['mq1']+mval['mq2']+gvdata['mresl']+gvdata['mress'])/gvdata['e0_kaon']**(3./2.)
-    Fss = gvdata['z0p_etas']*2*(mval['mq2']+gvdata['mress'])/gvdata['e0_etas']**(3./2.)
     if s['scale'] == 'PP':
         Lchi = 4 * np.pi * Fpi
     elif s['scale'] == 'PK':
@@ -51,7 +62,6 @@ def decay_constant(s,mval,gvdata):
     p = dict()
     p['mpi'] = gvdata['e0_pion']
     p['mka'] = gvdata['e0_kaon']
-    p['mss'] = gvdata['e0_etas']
     return {'x': x, 'y': y, 'p': p}
 
 def format_data(switches,data,mixed_data,hisq_params,priors):
@@ -60,7 +70,6 @@ def format_data(switches,data,mixed_data,hisq_params,priors):
     mpi  = list()
     mpiL = list()
     mka  = list()
-    mss  = list()
     aw0  = list()
     a2di = list()
     mju  = list()
@@ -74,9 +83,15 @@ def format_data(switches,data,mixed_data,hisq_params,priors):
         #print(ens)
         # get from postgre csv dump
         # get mres, E0 and Z0p
-        data2pt = data.sort_values(by='nbs').query("ensemble=='%s'" %e)[['e0_pion','z0p_pion','e0_kaon','z0p_kaon','e0_etas','z0p_etas','mresl','mress']].to_dict(orient='list')
+        data2pt = data.sort_values(by='nbs').query("ensemble=='%s'" %e)[['e0_pion','z0p_pion','e0_kaon','z0p_kaon','mresl','mress']].to_dict(orient='list')
         if ens in ['a12m220S','a12m220L']:
             datamix = {t:np.squeeze(mixed_data.sort_values(by='nbs').query("ensemble=='l3264f211b600m00507m0507m628' and tag=='%s'" %(t))[['E0']].as_matrix()) for t in ['phi_ju','phi_js','phi_ru','phi_rs']}
+        elif ens in ['a15m400','a15m350']:
+            datamix = {t:np.squeeze(mixed_data.sort_values(by='nbs').query("ensemble=='l1648f211b580m013m065m838' and tag=='%s'" %(t))[['E0']].as_matrix()) for t in ['phi_ju','phi_js','phi_ru','phi_rs']}
+        elif ens in ['a12m400','a12m350']:
+            datamix = {t:np.squeeze(mixed_data.sort_values(by='nbs').query("ensemble=='l2464f211b600m0102m0509m635' and tag=='%s'" %(t))[['E0']].as_matrix()) for t in ['phi_ju','phi_js','phi_ru','phi_rs']}
+        elif ens in ['a09m400','a09m350']:
+            datamix = {t:np.squeeze(mixed_data.sort_values(by='nbs').query("ensemble=='l3296f211b630m0074m037m440' and tag=='%s'" %(t))[['E0']].as_matrix()) for t in ['phi_ju','phi_js','phi_ru','phi_rs']}
         else:
             datamix = {t:np.squeeze(mixed_data.sort_values(by='nbs').query("ensemble=='%s' and tag=='%s'" %(e,t))[['E0']].as_matrix()) for t in ['phi_ju','phi_js','phi_ru','phi_rs']}
         datamerge = dict(data2pt,**datamix)
@@ -90,7 +105,6 @@ def format_data(switches,data,mixed_data,hisq_params,priors):
         mpiL.append(data_dict['p']['mpi'].mean * L_ens[ens])
         mjuL.append(gvdata['phi_ju'].mean * L_ens[ens])
         mka.append(data_dict['p']['mka'])
-        mss.append(data_dict['p']['mss'])
         mju.append(gvdata['phi_ju'])
         mjs.append(gvdata['phi_js'])
         mru.append(gvdata['phi_ru'])
@@ -102,7 +116,6 @@ def format_data(switches,data,mixed_data,hisq_params,priors):
         a2di.append(gv.gvar(hp['a2DI_mean'].iloc[0],hp['a2DI_sdev'].iloc[0])/r_a[ens]**2)
     priors['mpi'] = np.array(mpi)
     priors['mka'] = np.array(mka)
-    priors['mss'] = np.array(mss)
     priors['aw0'] = np.array(aw0)
     priors['mju'] = np.array(mju)
     priors['mjs'] = np.array(mjs)
