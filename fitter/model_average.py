59b3897a3077961ff7839799fd786ad240f0efdc
Nolan <nolan@unc.edu>
2019-10-23 08:46:11 -0400

    Added plot_comparison histogram to class model_average

--- fitter/model_average.py
+++ fitter/model_average.py
@@ -5,6 +5,7 @@ import time
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
+import scipy.stats as stats
 from collections import OrderedDict
 
 from fitter import fitter
@@ -36,12 +37,12 @@ class model_average(object):
         else:
             return param
 
-    def average(self, fit_results=None, param=None):
+    def average(self, param=None):
+        fit_results = self.fit_results
 
         if param is None:
             param = 'fit'
-        if fit_results is None:
-            fit_results = self.fit_results
+
 
         # Only get results that aren't None
         nonempty_keys = []
@@ -72,13 +73,13 @@ class model_average(object):
 
 
 
-    def plot_comparison(self, fit_results=None, param=None, other_results=None, title=None, xlabel=None,
+    def plot_comparison(self, param=None, other_results=None, title=None, xlabel=None,
                         show_model_avg=True):
+
+        fit_results = self.fit_results
 
         if param is None:
             param = 'fit'
-        if fit_results is None:
-            fit_results = self.fit_results
 
 
         if title is None:
@@ -142,12 +143,12 @@ class model_average(object):
                                      color=color, elinewidth=5.0, label=name.split('_')[0])
                         y = y + 1
                         labels = np.append(labels, str(name))
-                        
+
         ymax = y
 
         # Show model average
         if show_model_avg:
-            avg = self.average(fit_results, param)
+            avg = self.average(param)
             pm = lambda g, k : gv.mean(g) + k*gv.sdev(g)
             plt.axvspan(pm(avg, -1), pm(avg, +1), alpha=0.3, color='cornflowerblue', label='model avg')
             plt.axvline(pm(avg, 0), ls='-.', color='m')
@@ -303,4 +304,124 @@ class model_average(object):
         fig = plt.gcf()
         plt.close()
 
+        return fig
+
+
+    def plot_histogram(self, param, title=None, xlabel=None):
+
+        if title is None:
+            title = ""
+        if xlabel is None:
+            xlabel = self._param_keys_dict(param)
+
+        # read Bayes Factors
+        logGBF_list = [self.fit_results[model]['logGBF'] for model in self.fit_results.keys()]
+
+        # initiate a bunch of parameters
+        y = 0
+        y_list = []
+        y_dict = dict()
+
+        # weights
+        w_lst = []
+        wd = dict()
+
+        # p. dist. fcn
+        pdf = 0
+        pdfdict = dict()
+
+        # c. dist. fcn.
+        cdf = 0
+        cdfdict = dict()
+
+
+        # for plotting
+        temp_array = np.array([self.fit_results[model][param] for model in self.fit_results.keys()])
+        temp_array = gv.gvar(temp_array[temp_array != 'nan'])
+        pm_arr = lambda arr, k : gv.mean(temp_array) + k *gv.sdev(temp_array)
+        min_x = np.min(pm_arr(temp_array, -2))
+        max_x = np.max(pm_arr(temp_array, 2))
+        x = np.linspace(min_x, max_x, 2000)
+
+        for model in self.fit_results.keys():
+            if self.fit_results[model][param] is not np.nan:
+                r = gv.gvar(self.fit_results[model][param])
+                y_dict[model] = r
+
+                w = 1/sum(np.exp(np.array(logGBF_list)-self.fit_results[model]['logGBF']))
+                sqrtw = np.sqrt(w) # sqrt scales the std dev correctly
+                wd[model] = w
+                w_lst.append(w)
+
+                y += gv.gvar(w*r.mean,sqrtw*r.sdev)
+                y_list.append(r.mean)
+
+                p = stats.norm.pdf(x,r.mean,r.sdev)
+                pdf += w*p
+                pdfdict[model] = w*p
+
+
+                c = stats.norm.cdf(x,r.mean,r.sdev)
+                cdf += w*c
+                cdfdict[model] = w*c
+
+        y_list = np.array(y_list)
+        w_lst = np.array(w_lst)
+
+        plot_params = {'x':x, 'pdf':pdf, 'pdfdict':pdfdict, 'cdf':cdf, 'cdfdict':cdfdict}
+
+        gr = 1.618034333
+        fs2_base = 3.50394
+        plt_axes = [0.14,0.165,0.825,0.825]
+        fig_size2 = (fs2_base,fs2_base/gr)
+        lw = 0.5
+        fs_l = 15
+        fs_xy = 24
+        ts = 15
+
+        x = plot_params['x']
+        ysum = plot_params['pdf']
+
+        ydict = plot_params['pdfdict']
+        cdf = plot_params['cdf']
+
+        # '-','--','-.',':'
+        # #ec5d57 #70bf41 #51a7f9
+
+        fig = plt.figure('result histogram')#,figsize=fig_size2)
+        ax = plt.axes(plt_axes)
+        ax.fill_between(x=x,y1=ysum,facecolor='#b36ae2',edgecolor='black',alpha=0.4,label='model average')
+        # get 95% confidence
+        lidx95 = abs(cdf-0.025).argmin()
+        uidx95 = abs(cdf-0.975).argmin()
+        ax.fill_between(x=x[lidx95:uidx95],y1=ysum[lidx95:uidx95],facecolor='#b36ae2',edgecolor='black',alpha=0.4)
+        # get 68% confidence
+        lidx68 = abs(cdf-0.158655254).argmin()
+        uidx68 = abs(cdf-0.841344746).argmin()
+        ax.fill_between(x=x[lidx68:uidx68],y1=ysum[lidx68:uidx68],facecolor='#b36ae2',edgecolor='black',alpha=0.4)
+        # plot black curve over
+        ax.errorbar(x=[x[lidx95],x[lidx95]],y=[0,ysum[lidx95]],color='black',lw=lw)
+        ax.errorbar(x=[x[uidx95],x[uidx95]],y=[0,ysum[uidx95]],color='black',lw=lw)
+        ax.errorbar(x=[x[lidx68],x[lidx68]],y=[0,ysum[lidx68]],color='black',lw=lw)
+        ax.errorbar(x=[x[uidx68],x[uidx68]],y=[0,ysum[uidx68]],color='black',lw=lw)
+
+        ax.errorbar(x=x,y=ysum,ls='-',color='black',lw=lw)
+
+        for a in ydict.keys():
+            ax.plot(x,ydict[a])
+
+        leg = ax.legend(fontsize=fs_l, edgecolor='k',fancybox=False)
+        ax.set_ylim(bottom=0)
+        #ax.set_xlim([1.225,1.335])
+        ax.set_xlabel(xlabel, fontsize=fs_xy)
+        frame = plt.gca()
+        frame.axes.get_yaxis().set_visible(False)
+        ax.xaxis.set_tick_params(labelsize=ts,width=lw)
+
+        # legend line width
+        [ax.spines[key].set_linewidth(lw) for key in ax.spines]
+        leg.get_frame().set_linewidth(lw)
+
+        fig = plt.gcf()
+        plt.close()
         return fig
