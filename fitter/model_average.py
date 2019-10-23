48ce27d266fe21de8a4feec97c7c318dbb6585fc
Nolan <nolan@unc.edu>
2019-10-23 15:59:15 -0400

    Improved histogram plot; added script for generating avg figures

--- fitter/model_average.py
+++ fitter/model_average.py
@@ -14,6 +14,23 @@ class model_average(object):
     def __init__(self, fit_results):
         self.fit_results = fit_results
 
+    def _get_model_info_from_name(self, name):
+        output_dict = {}
+        output_dict['base'] = name.split('_')[0]
+        output_dict['F2'] = name.split('_')[1]
+        output_dict['include_alphaS'] = False
+        output_dict['include_FV'] = False
+        output_dict['include_logSq'] = False
+
+        if '_FV' in name:
+            output_dict['include_FV'] = True
+        if '_alphaS' in name:
+            output_dict['include_alphaS'] = True
+        if '_logSq' in name:
+            output_dict['include_logSq'] = True
+
+        return output_dict
+
     def _param_keys_dict(self, param):
         if param == 'fit':
             return '$F_K/F_\pi$'
@@ -34,38 +51,43 @@ class model_average(object):
         elif param == 'A_loga':
             return '$A_{\log a}$'
 
+        elif param == 'FKFK':
+            return '$F_K^2$'
+        elif param == 'FKFpi':
+            return '$F_K F_\pi$'
+        elif param == 'FpiFpi':
+            return '$F_\pi^2$'
+
         else:
             return param
 
     def average(self, param=None):
-        fit_results = self.fit_results
-
         if param is None:
             param = 'fit'
 
 
         # Only get results that aren't None
         nonempty_keys = []
-        for model in fit_results.keys():
-            if (fit_results[model][param] is not np.nan) and (fit_results[model][param] is not None):
+        for model in self.fit_results.keys():
+            if (self.fit_results[model][param] is not np.nan) and (self.fit_results[model][param] is not None):
                 nonempty_keys.append(model)
 
         # calculate P( M_k | D )
         prob_Mk_given_D = lambda model_k : (
-             np.exp(fit_results[model_k]['logGBF']) / np.sum([np.exp(fit_results[model_l]['logGBF']) for model_l in nonempty_keys])
+             np.exp(self.fit_results[model_k]['logGBF']) / np.sum([np.exp(self.fit_results[model_l]['logGBF']) for model_l in nonempty_keys])
         )
 
         # Get central value
         expct_y = 0
         for model in nonempty_keys:
-            expct_y += gv.mean(gv.gvar(fit_results[model][param])) *prob_Mk_given_D(model)
+            expct_y += gv.mean(gv.gvar(self.fit_results[model][param])) *prob_Mk_given_D(model)
 
         # Get variance
         var_y = 0
         for model in nonempty_keys:
-            var_y += gv.var(gv.gvar(fit_results[model][param])) *prob_Mk_given_D(model)
+            var_y += gv.var(gv.gvar(self.fit_results[model][param])) *prob_Mk_given_D(model)
         for model in nonempty_keys:
-            var_y += (gv.mean(gv.gvar(fit_results[model][param])))**2 *prob_Mk_given_D(model)
+            var_y += (gv.mean(gv.gvar(self.fit_results[model][param])))**2 *prob_Mk_given_D(model)
 
         var_y -= (expct_y)**2
 
@@ -75,8 +97,6 @@ class model_average(object):
 
     def plot_comparison(self, param=None, other_results=None, title=None, xlabel=None,
                         show_model_avg=True):
-
-        fit_results = self.fit_results
 
         if param is None:
             param = 'fit'
@@ -90,7 +110,7 @@ class model_average(object):
         colors = ['salmon', 'darkorange', 'mediumaquamarine', 'orchid', 'silver']
         markers = ['^', 'o', 'v', '*']
 
-        results_array = [fit_results]
+        results_array = [self.fit_results]
         if other_results is not None:
             results_array.append(other_results)
 
@@ -115,23 +135,24 @@ class model_average(object):
 
                     else:
                         # Color by base model
-                        if 'ma_' in name:
+                        model_info = self._get_model_info_from_name(name)
+                        if model_info['base'] == 'ma':
                             color = colors[0]
-                        elif 'xpt_' in name:
+                        elif model_info['base'] == 'xpt':
                             color = colors[1]
-                        elif 'xpt-ratio_' in name:
+                        elif model_info['base'] == 'xpt-ratio':
                             color = colors[2]
-                        elif 'ma-ratio_' in name:
+                        elif model_info['base'] == 'ma-ratio':
                             color = colors[3]
                         else:
                             color = colors[4]
 
                         # Marker by F^2
-                        if '_FKFK_' in name:
+                        if model_info['F2'] == 'FKFK':
                             marker = markers[0]
-                        elif '_FKFpi_' in name:
+                        elif model_info['F2'] == 'FKFpi':
                             marker = markers[1]
-                        elif '_FpiFpi_' in name:
+                        elif model_info['F2'] == 'FpiFpi':
                             marker = markers[2]
                         else:
                             marker = markers[3]
@@ -172,30 +193,31 @@ class model_average(object):
         ax_logGBF = plt.axes([0.60,0.10,0.09,0.8])
 
         # Get max logGBF
-        logGBF_max = np.nanmax([gv.mean(gv.gvar(fit_results[model]['logGBF']))
-                               for model in fit_results.keys()])
+        logGBF_max = np.nanmax([gv.mean(gv.gvar(self.fit_results[model]['logGBF']))
+                               for model in self.fit_results.keys()])
 
         y=0
         labels = np.array([])
-        for results in [fit_results]:
+        for results in [self.fit_results]:
             for name in results.keys():
-                if 'ma_' in name:
+                model_info = self._get_model_info_from_name(name)
+                if model_info['base'] == 'ma':
                     color = colors[0]
-                elif 'xpt_' in name:
+                elif model_info['base'] == 'xpt':
                     color = colors[1]
-                elif 'xpt-ratio_' in name:
+                elif model_info['base'] == 'xpt-ratio':
                     color = colors[2]
-                elif 'ma-ratio_' in name:
+                elif model_info['base'] == 'ma-ratio':
                     color = colors[3]
                 else:
                     color = colors[4]
 
                 # Marker by F^2
-                if '_FKFK_' in name:
+                if model_info['F2'] == 'FKFK':
                     marker = markers[0]
-                elif '_FKFpi_' in name:
+                elif model_info['F2'] == 'FKFpi':
                     marker = markers[1]
-                elif '_FpiFpi_' in name:
+                elif model_info['F2'] == 'FpiFpi':
                     marker = markers[2]
                 else:
                     marker = markers[3]
@@ -221,25 +243,26 @@ class model_average(object):
 
         y=0
         labels = np.array([])
-        for results in [fit_results]:
+        for results in [self.fit_results]:
             for name in results.keys():
-                if 'ma_' in name:
+                model_info = self._get_model_info_from_name(name)
+                if model_info['base'] == 'ma':
                     color = colors[0]
-                elif 'xpt_' in name:
+                elif model_info['base'] == 'xpt':
                     color = colors[1]
-                elif 'xpt-ratio_' in name:
+                elif model_info['base'] == 'xpt-ratio':
                     color = colors[2]
-                elif 'ma-ratio_' in name:
+                elif model_info['base'] == 'ma-ratio':
                     color = colors[3]
                 else:
                     color = colors[4]
 
                 # Marker by F^2
-                if '_FKFK_' in name:
+                if model_info['F2'] == 'FKFK':
                     marker = markers[0]
-                elif '_FKFpi_' in name:
+                elif model_info['F2'] == 'FKFpi':
                     marker = markers[1]
-                elif '_FpiFpi_' in name:
+                elif model_info['F2'] == 'FpiFpi':
                     marker = markers[2]
                 else:
                     marker = markers[3]
@@ -264,25 +287,26 @@ class model_average(object):
 
         y=0
         labels = np.array([])
-        for results in [fit_results]:
+        for results in [self.fit_results]:
             for name in results.keys():
-                if 'ma_' in name:
+                model_info = self._get_model_info_from_name(name)
+                if model_info['base'] == 'ma':
                     color = colors[0]
-                elif 'xpt_' in name:
+                elif model_info['base'] == 'xpt':
                     color = colors[1]
-                elif 'xpt-ratio_' in name:
+                elif model_info['base'] == 'xpt-ratio':
                     color = colors[2]
-                elif 'ma-ratio_' in name:
+                elif model_info['base'] == 'ma-ratio':
                     color = colors[3]
                 else:
                     color = colors[4]
 
                 # Marker by F^2
-                if '_FKFK_' in name:
+                if model_info['F2'] == 'FKFK':
                     marker = markers[0]
-                elif '_FKFpi_' in name:
+                elif model_info['F2'] == 'FKFpi':
                     marker = markers[1]
-                elif '_FpiFpi_' in name:
+                elif model_info['F2'] == 'FpiFpi':
                     marker = markers[2]
                 else:
                     marker = markers[3]
@@ -314,113 +338,117 @@ class model_average(object):
         if xlabel is None:
             xlabel = self._param_keys_dict(param)
 
-        # read Bayes Factors
-        logGBF_list = [self.fit_results[model]['logGBF'] for model in self.fit_results.keys()]
+        colors = ['salmon', 'darkorange', 'mediumaquamarine', 'orchid', 'silver']
+        for j, F2 in enumerate(['All', 'FKFK', 'FKFpi', 'FpiFpi']):
 
-        # initiate a bunch of parameters
-        y = 0
-        y_list = []
-        y_dict = dict()
+            # read Bayes Factors
+            logGBF_list = [self.fit_results[model]['logGBF'] for model in self.fit_results.keys()]
 
-        # weights
-        w_lst = []
-        wd = dict()
+            # initiate a bunch of parameters
+            y = 0
+            y_list = []
+            y_dict = dict()
 
-        # p. dist. fcn
-        pdf = 0
-        pdfdict = dict()
+            # weights
+            w_lst = []
+            wd = dict()
 
-        # c. dist. fcn.
-        cdf = 0
-        cdfdict = dict()
+            # p. dist. fcn
+            pdf = 0
+            pdfdict = dict()
 
-
-        # for plotting
-        temp_array = np.array([self.fit_results[model][param] for model in self.fit_results.keys()])
-        temp_array = gv.gvar(temp_array[temp_array != 'nan'])
-        pm_arr = lambda arr, k : gv.mean(temp_array) + k *gv.sdev(temp_array)
-        min_x = np.min(pm_arr(temp_array, -2))
-        max_x = np.max(pm_arr(temp_array, 2))
-        x = np.linspace(min_x, max_x, 2000)
-
-        for model in self.fit_results.keys():
-            if self.fit_results[model][param] is not np.nan:
-                r = gv.gvar(self.fit_results[model][param])
-                y_dict[model] = r
-
-                w = 1/sum(np.exp(np.array(logGBF_list)-self.fit_results[model]['logGBF']))
-                sqrtw = np.sqrt(w) # sqrt scales the std dev correctly
-                wd[model] = w
-                w_lst.append(w)
-
-                y += gv.gvar(w*r.mean,sqrtw*r.sdev)
-                y_list.append(r.mean)
-
-                p = stats.norm.pdf(x,r.mean,r.sdev)
-                pdf += w*p
-                pdfdict[model] = w*p
+            # c. dist. fcn.
+            cdf = 0
+            cdfdict = dict()
 
 
-                c = stats.norm.cdf(x,r.mean,r.sdev)
-                cdf += w*c
-                cdfdict[model] = w*c
+            # for plotting
+            temp_array = np.array([self.fit_results[model][param] for model in self.fit_results.keys()])
+            temp_array = gv.gvar(temp_array[temp_array != 'nan'])
+            pm_arr = lambda arr, k : gv.mean(temp_array) + k *gv.sdev(temp_array)
+            min_x = np.min(pm_arr(temp_array, -2))
+            max_x = np.max(pm_arr(temp_array, 2))
+            x = np.linspace(min_x, max_x, 2000)
 
-        y_list = np.array(y_list)
-        w_lst = np.array(w_lst)
 
-        plot_params = {'x':x, 'pdf':pdf, 'pdfdict':pdfdict, 'cdf':cdf, 'cdfdict':cdfdict}
+            for model in self.fit_results.keys():
+                model_info = self._get_model_info_from_name(model)
+                if self.fit_results[model][param] is not np.nan and (model_info['F2'] == F2 or F2=='All'):
+                    r = gv.gvar(self.fit_results[model][param])
+                    y_dict[model] = r
 
-        gr = 1.618034333
-        fs2_base = 3.50394
-        plt_axes = [0.14,0.165,0.825,0.825]
-        fig_size2 = (fs2_base,fs2_base/gr)
-        lw = 0.5
-        fs_l = 15
-        fs_xy = 24
-        ts = 15
+                    w = 1/sum(np.exp(np.array(logGBF_list)-self.fit_results[model]['logGBF']))
+                    sqrtw = np.sqrt(w) # sqrt scales the std dev correctly
+                    wd[model] = w
+                    w_lst.append(w)
 
-        x = plot_params['x']
-        ysum = plot_params['pdf']
+                    y += gv.gvar(w*r.mean,sqrtw*r.sdev)
+                    y_list.append(r.mean)
 
-        ydict = plot_params['pdfdict']
-        cdf = plot_params['cdf']
+                    p = stats.norm.pdf(x,r.mean,r.sdev)
+                    pdf += w*p
+                    pdfdict[model] = w*p
 
-        # '-','--','-.',':'
-        # #ec5d57 #70bf41 #51a7f9
 
-        fig = plt.figure('result histogram')#,figsize=fig_size2)
-        ax = plt.axes(plt_axes)
-        ax.fill_between(x=x,y1=ysum,facecolor='#b36ae2',edgecolor='black',alpha=0.4,label='model average')
-        # get 95% confidence
-        lidx95 = abs(cdf-0.025).argmin()
-        uidx95 = abs(cdf-0.975).argmin()
-        ax.fill_between(x=x[lidx95:uidx95],y1=ysum[lidx95:uidx95],facecolor='#b36ae2',edgecolor='black',alpha=0.4)
-        # get 68% confidence
-        lidx68 = abs(cdf-0.158655254).argmin()
-        uidx68 = abs(cdf-0.841344746).argmin()
-        ax.fill_between(x=x[lidx68:uidx68],y1=ysum[lidx68:uidx68],facecolor='#b36ae2',edgecolor='black',alpha=0.4)
-        # plot black curve over
-        ax.errorbar(x=[x[lidx95],x[lidx95]],y=[0,ysum[lidx95]],color='black',lw=lw)
-        ax.errorbar(x=[x[uidx95],x[uidx95]],y=[0,ysum[uidx95]],color='black',lw=lw)
-        ax.errorbar(x=[x[lidx68],x[lidx68]],y=[0,ysum[lidx68]],color='black',lw=lw)
-        ax.errorbar(x=[x[uidx68],x[uidx68]],y=[0,ysum[uidx68]],color='black',lw=lw)
+                    c = stats.norm.cdf(x,r.mean,r.sdev)
+                    cdf += w*c
+                    cdfdict[model] = w*c
 
-        ax.errorbar(x=x,y=ysum,ls='-',color='black',lw=lw)
+            y_list = np.array(y_list)
+            w_lst = np.array(w_lst)
 
-        for a in ydict.keys():
-            ax.plot(x,ydict[a])
+            plot_params = {'x':x, 'pdf':pdf, 'pdfdict':pdfdict, 'cdf':cdf, 'cdfdict':cdfdict}
 
-        leg = ax.legend(fontsize=fs_l, edgecolor='k',fancybox=False)
-        ax.set_ylim(bottom=0)
-        #ax.set_xlim([1.225,1.335])
-        ax.set_xlabel(xlabel, fontsize=fs_xy)
-        frame = plt.gca()
-        frame.axes.get_yaxis().set_visible(False)
-        ax.xaxis.set_tick_params(labelsize=ts,width=lw)
+            gr = 1.618034333
+            fs2_base = 3.50394
+            lw = 0.5
+            fs_l = 15
+            fs_xy = 24
+            ts = 15
 
-        # legend line width
-        [ax.spines[key].set_linewidth(lw) for key in ax.spines]
-        leg.get_frame().set_linewidth(lw)
+            x = plot_params['x']
+            ysum = plot_params['pdf']
+
+            ydict = plot_params['pdfdict']
+            cdf = plot_params['cdf']
+
+            # '-','--','-.',':'
+            # #ec5d57 #70bf41 #51a7f9
+
+            fig = plt.figure('result histogram')#,figsize=fig_size2)
+            ax = plt.axes()
+            ax.fill_between(x=x,y1=ysum,facecolor=colors[j], edgecolor='black',alpha=0.4,label=self._param_keys_dict(F2))
+            if F2 == 'All':
+                # get 95% confidence
+                lidx95 = abs(cdf-0.025).argmin()
+                uidx95 = abs(cdf-0.975).argmin()
+                ax.fill_between(x=x[lidx95:uidx95],y1=ysum[lidx95:uidx95],facecolor=colors[j],edgecolor='black',alpha=0.4)
+                # get 68% confidence
+                lidx68 = abs(cdf-0.158655254).argmin()
+                uidx68 = abs(cdf-0.841344746).argmin()
+                ax.fill_between(x=x[lidx68:uidx68],y1=ysum[lidx68:uidx68],facecolor=colors[j],edgecolor='black',alpha=0.4)
+                # plot black curve over
+                ax.errorbar(x=[x[lidx95],x[lidx95]],y=[0,ysum[lidx95]],color='black',lw=lw)
+                ax.errorbar(x=[x[uidx95],x[uidx95]],y=[0,ysum[uidx95]],color='black',lw=lw)
+                ax.errorbar(x=[x[lidx68],x[lidx68]],y=[0,ysum[lidx68]],color='black',lw=lw)
+                ax.errorbar(x=[x[uidx68],x[uidx68]],y=[0,ysum[uidx68]],color='black',lw=lw)
+
+                ax.errorbar(x=x,y=ysum,ls='-',color='black',lw=lw)
+
+            for a in ydict.keys():
+                ax.plot(x,ydict[a], color=colors[j])
+
+            leg = ax.legend(fontsize=fs_l, edgecolor='k',fancybox=False)
+            ax.set_ylim(bottom=0)
+            #ax.set_xlim([1.225,1.335])
+            ax.set_xlabel(xlabel, fontsize=fs_xy)
+            frame = plt.gca()
+            frame.axes.get_yaxis().set_visible(False)
+            ax.xaxis.set_tick_params(labelsize=ts,width=lw)
+
+            # legend line width
+            [ax.spines[key].set_linewidth(lw) for key in ax.spines]
+            leg.get_frame().set_linewidth(lw)
 
         fig = plt.gcf()
         plt.close()
---
+++ scripts/make_fig_averages.py
@@ -0,0 +1,36 @@
+import numpy as np
+import gvar as gv
+import lsqfit
+import sys
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import time
+
+sys.path.append("../")
+from fitter import data_loader as dl
+from fitter import misc as ms
+from fitter import model_average as md
+
+for j in range(10): # Sometimes this needs to be loaded twice...
+    matplotlib.rcParams['figure.figsize'] = [10, 10]
+
+data_loader = dl.data_loader()
+fit_results = data_loader.get_fit_info('fit_results.csv')
+other_results = data_loader.get_fit_info('other_results.csv')
+
+model_average = md.model_average(fit_results)
+
+fig = model_average.plot_histogram('fit')
+data_loader.save_plots(fig, output_filename='histogram_fit')
+
+fig = model_average.plot_histogram('delta_su2')
+data_loader.save_plots(fig, output_filename='histogram_delta_su2')
+
+fig = model_average.plot_comparison(other_results=other_results, param='fit')
+data_loader.save_plots(fig, output_filename='comparison_fits')
+
+fig = model_average.plot_comparison(other_results=other_results, param='delta_su2')
+data_loader.save_plots(fig, output_filename='comparison_delta_su2')
