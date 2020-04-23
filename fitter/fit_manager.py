f103a651e024b8bb1a87f99d2906c286e565fd3f
Nolan <nolan@unc.edu>
2020-04-23 11:04:09 -0400

    Change default n3lo priors (~100 -> ~10)

--- fitter/fit_manager.py
+++ fitter/fit_manager.py
@@ -108,11 +108,11 @@ class fit_manager(object):
             prior['A_p'] = gv.gvar('0.0(10.0)')#gv.gvar('0.0(5.0)')
 
             # n3lo terms
-            prior['A_ak'] = gv.gvar('0.0(100.0)')
-            prior['A_ap'] = gv.gvar('0.0(100.0)')
-            prior['A_kk'] = gv.gvar('0.0(100.0)')
-            prior['A_kp'] = gv.gvar('0.0(100.0)')
-            prior['A_pp'] = gv.gvar('0.0(100.0)')
+            prior['A_ak'] = gv.gvar('0.0(10.0)')
+            prior['A_ap'] = gv.gvar('0.0(10.0)')
+            prior['A_kk'] = gv.gvar('0.0(10.0)')
+            prior['A_kp'] = gv.gvar('0.0(10.0)')
+            prior['A_pp'] = gv.gvar('0.0(10.0)')
 
         # Convert bootstrapped data into gvar data
         bias_corrector = lambda arr : arr[1:] + (arr[0] - np.mean(arr[1:]))
